be85830f114a713231475a68b92d9f51cc92eb0c
Nuno Costa <nm.cdc@hotmail.com>
2021-11-17 23:19:20 +0000

    added restaurant_controller

--- db/migrate/20211117230233_create_reviews.rb
+++ db/migrate/20211117230233_create_reviews.rb
@@ -3,12 +3,9 @@ class CreateReviews < ActiveRecord::Migration[6.1]
     create_table :reviews do |t|
       t.string :content
       t.integer :rating
-      t.integer :restaurant_id
-      t.string :restaurant_id
-      t.index_reviews_on_restaurant_id :name
+      t.references :restaurant, null: false, foreign_key: true
 
       t.timestamps
     end
-    add_index :reviews, :restaurant_id
   end
 end
